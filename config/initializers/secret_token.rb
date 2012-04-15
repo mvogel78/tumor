912961674a53c161cf4ff820655789215e2ce577
mandy <mandy.vogel@medizin.uni-leipzig.de>
2012-04-15 11:12:13 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tumor::Application.config.secret_token = '8ffb8616ac99da44e8aeb3aa977bc1dc4175f91c61bdca5011514fd3277bc66daa5d98e1289a862f05de37ab6d7fb8613a3fbc554dec00db85f56ef1618595cf'
